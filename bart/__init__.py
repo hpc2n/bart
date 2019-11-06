43f0176fc872e1a40bf773134ef0a3c79635dd9d
Magnus Jonsson <magnus@hpc2n.umu.se>
2019-11-06 13:25:44 +0100

    Updated release version.

--- bart/__init__.py
+++ bart/__init__.py
@@ -4,5 +4,5 @@ import time
 gmt = time.gmtime()
 
 # set this to "correct" version when making a release
-__version__ = 'svn-%04d%02d%02d' % (gmt.tm_year, gmt.tm_mon, gmt.tm_mday)
+__version__ = '008-sams-20191105'
 
